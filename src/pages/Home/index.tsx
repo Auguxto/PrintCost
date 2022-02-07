01165cdf9c43a1a5fe86a4eebdaca30665348f10
Victor Augusto Ferreira <victoraugusferreira@gmail.com>
2022-02-07 12:01:51 -0300

    Init project

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <h1>PrintCost</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae ratione
+        unde perferendis porro, sequi, repellendus qui explicabo consequatur
+        deserunt nostrum ducimus? Eveniet excepturi quas dolore dolorum
+        adipisci, distinctio reprehenderit consectetur
+      </p>
+    </>
+  );
+};
+
+export default Home;
